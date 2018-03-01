3e8b871e7f0ad56e87a828a2105edbde203b9047
Scott Fujimoto <12649111+sfujim@users.noreply.github.com>
2018-03-01 16:06:32 -0500

    bug in script

--- run_experiments.sh
+++ run_experiments.sh
@@ -4,45 +4,45 @@
 
 for ((i=0;i<10;i+=1))
 do 
-	python2 new_main.py \
+	python2 main.py \
 	--policy_name "TD3" \
 	--env_name "HalfCheetah-v1" \
 	--seed $i \
-	--start_timesteps 10000 \
+	--start_timesteps 10000
 
-	python2 new_main.py \
+	python2 main.py \
 	--policy_name "TD3" \
 	--env_name "Hopper-v1" \
 	--seed $i \
-	--start_timesteps 1000 \
+	--start_timesteps 1000
 
-	python2 new_main.py \
+	python2 main.py \
 	--policy_name "TD3" \
 	--env_name "Walker2d-v1" \
 	--seed $i \
-	--start_timesteps 1000 \
+	--start_timesteps 1000
 
-	python2 new_main.py \
+	python2 main.py \
 	--policy_name "TD3" \
 	--env_name "Ant-v1" \
 	--seed $i \
-	--start_timesteps 10000 \
+	--start_timesteps 10000
 
-	python2 new_main.py \
+	python2 main.py \
 	--policy_name "TD3" \
 	--env_name "InvertedPendulum-v1" \
 	--seed $i \
-	--start_timesteps 1000 \
+	--start_timesteps 1000
 
-	python2 new_main.py \
+	python2 main.py \
 	--policy_name "TD3" \
 	--env_name "InvertedDoublePendulum-v1" \
 	--seed $i \
-	--start_timesteps 1000 \
+	--start_timesteps 1000
 
-	python2 new_main.py \
+	python2 main.py \
 	--policy_name "TD3" \
 	--env_name "Reacher-v1" \
 	--seed $i \
-	--start_timesteps 1000 \
+	--start_timesteps 1000
 done
