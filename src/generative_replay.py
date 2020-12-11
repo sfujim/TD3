8112d9941ba9f7563bc175295988149a6ff8a2d3
Baris Imre <barisimre98@gmail.com>
2020-12-11 14:26:57 +0100

    Keep adding to generative replay

--- src/generative_replay.py
+++ src/generative_replay.py
@@ -2,20 +2,43 @@ import torch
 from torch import nn, optim
 from torch.nn import functional as F
 import numpy as np
+import gym
+from tqdm import tqdm
 
 BATCH_SIZE = 64
 EPOCHS = 50
 INPUT_SIZE = 11
 LAYER_SIZE = 10
-LATENT_SIZE = 3   
+LATENT_SIZE = 3 
+LEARNING_RATE = 0.001
+BUFFER_SIZE = 1024
+
+# Env sizes
+state_dim = 4
+action_dim = 1
+action_low = -3.0
+action_high = 3.0
+state_low = -15
+state_high = 15
+
+
+# Make CPU if cuda doesnt work
+device = "cuda"  
 
 
 class GenerativeReplay():
     def __init__(self):
-        pass
+        # Model
+        self.model = VAE().to(device)
+        self.opt = optim.Adam(self.model.parameters(), lr=LEARNING_RATE)
+        self.buffer = []      
 
-    def add():
-        pass
+    def add(self, experience):
+        self.buffer.append(experience)
+        if len(self.buffer) >= BUFFER_SIZE:
+            self.train()
+            self.test()
+            self.buffer = []
 
     def sample():
         pass
@@ -26,7 +49,19 @@ class GenerativeReplay():
     def test():
         pass
 
+    def normalize(experience):
+        # [s0, s1, s2, s3, a, s0, s1, s2, s3, r, d]
+        res = []
 
+    def normalize_action(x):
+        return (x-action_low)/(action_high-action_low)
+    def normalize_state(s):
+        res = []
+        for i in s:
+            res.append((i-state_low)/(state_high-state_low))
+        return res
+    def normalize_reward(r):
+        return np.array([r/20.0])   
 
 
 
@@ -62,4 +97,7 @@ class VAE(nn.Module):
         z = self.reparameterize(mu, sigma)
         return self.decode(z), mu, sigma
 
-
+    def loss_function(recon_x, x, mu, sigma):
+        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
+        KLD = -0.5 * torch.sum(1 + sigma - mu.pow(2) - sigma.exp())
+        return BCE + KLD
