34770ccdcb51df6f6c7d85c1ba7e40d71b940d16
Scott Fujimoto <12649111+sfujim@users.noreply.github.com>
2023-07-14 15:40:36 -0400

    Correct type error with argpase

--- main.py
+++ main.py
@@ -41,10 +41,10 @@ if __name__ == "__main__":
 	parser.add_argument("--start_timesteps", default=25e3, type=int)# Time steps initial random policy is used
 	parser.add_argument("--eval_freq", default=5e3, type=int)       # How often (time steps) we evaluate
 	parser.add_argument("--max_timesteps", default=1e6, type=int)   # Max time steps to run environment
-	parser.add_argument("--expl_noise", default=0.1)                # Std of Gaussian exploration noise
+	parser.add_argument("--expl_noise", default=0.1, type=float)    # Std of Gaussian exploration noise
 	parser.add_argument("--batch_size", default=256, type=int)      # Batch size for both actor and critic
-	parser.add_argument("--discount", default=0.99)                 # Discount factor
-	parser.add_argument("--tau", default=0.005)                     # Target network update rate
+	parser.add_argument("--discount", default=0.99, type=float)     # Discount factor
+	parser.add_argument("--tau", default=0.005, type=float)         # Target network update rate
 	parser.add_argument("--policy_noise", default=0.2)              # Noise added to target policy during critic update
 	parser.add_argument("--noise_clip", default=0.5)                # Range to clip target policy noise
 	parser.add_argument("--policy_freq", default=2, type=int)       # Frequency of delayed policy updates
